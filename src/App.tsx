294a2e2808845601c43dcffef04c2d4c52f783bd
Onur Eymen <onurceymen@yahoo.com>
2024-10-13 22:39:58 +0300

    istatistik bölümü yorum satırına alındı (hata giderimi devam3)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./Assets/header/Header"
 import Download from "./Assets/hemen indir/download"
 import Hero from "./Assets/hero Box/Hero"
 import Why from "./Assets/Neden Tedarika/why"
-import Statistic from "./Assets/rakamlarla tedarika/statistic"
+//import Statistic from "./Assets/rakamlarla tedarika/statistic"
 import Vision from "./Assets/vizyonumuz/vision"
 import Solution from "./Assets/Çözümlerimiz/Solution"
 
@@ -15,7 +15,6 @@ function App() {
       <Hero />
       <Why />
       <Solution />
-      <Statistic />
       <Vision />
       <Download />
       <Footer />
